212566f11cfe2b8a0904e3553eee89e1cd576cc3
Johannes Baiter <johannes.baiter@gmail.com>
2014-01-19 22:56:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from setuptools import setup, Extension
 
 setup(name='epeg-cffi',
     version="0.01",
+    description=(
+        "Insanely fast JPEG/ JPG thumbnail scaling with the minimum fuss and "
+        "CPU overhead. It makes use of libjpeg features of being able to load "
+        "an image by only decoding the DCT coefficients needed to reconstruct "
+        "an image of the size desired."),
+    long_description=open('README.rst').read(),
     author="Johannes Baiter",
     url="http://github.com/jbaiter/epeg-cffi.git",
     author_email="johannes.baiter@gmail.com",
+    license='MIT',
     py_modules=['epeg'],
     ext_modules=[
         Extension('_epeg',
